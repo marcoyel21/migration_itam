c332c3d31c43de04a5ba3c9bf158f19d5023de9d
Roberto González Téllez <73495438+RobertoGlz@users.noreply.github.com>
2022-05-20 12:16:21 -0500

    Add file for creating education and gender tables

--- salvador_tables.R
+++ salvador_tables.R
@@ -21,26 +21,32 @@ options(scipen = 99999)
 #### Read in datasets: EHPM for years 2020 and 2010 #### ----------------------
 salvador_20 <- read_spss(here("data", "EHPM", "EHPM 2020.sav"))
 
-# 2010 is more complicated since there are many files so it's easier to 
-# load them all and bind_rows and then process the single appended dataset
-temp_files <- list.files(path = here("data", "EHPM", "EHPM_2010"), 
-                         pattern = "*.sav",
-                         full.names = TRUE)
-# Garbage collect to save memory since data is large
-gc(full = TRUE)
+salvador_10_a <- read_sav(here("data", "EHPM", "EHPM_2010", "SEC021.sav")) %>% 
+  mutate(id = row_number()) %>%
+  select(id, R217A)
 
-# read in data and bind rows
-salvador_10 <- lapply(temp_files, read_spss) %>%
-  bind_rows()
+salvador_10_b <- read_sav(here("data", "EHPM", "EHPM_2010", "SEC01.sav")) %>% 
+  mutate(id = row_number()) %>%
+  select(id, FAC01, R104, R106)
 
-#### Select variables of interest for computation efficiency #### -------------
+salvador_10 <- inner_join(salvador_10_a, salvador_10_b,
+                          by = "id")
+
+#### Select variables of interest for computation efficiency #### -------------
 salvador_relevant_20 <- salvador_20 %>%
-  select(fac00, r104, r106, r204, r204g) %>%
+  select(fac00, r104, r106, r204, r204g, actpr) %>%
   rename(factor = fac00,
          sexo = r104,
          edad = r106,
          nivel = r204,
-         grado = r204g)
+         grado = r204g,
+         ocupacion = actpr)
+
+salvador_relevant_10 <- salvador_10 %>%
+  rename(factor = FAC01,
+         sexo = R104,
+         edad = R106,
+         nivel = R217A)
 
 #### Impute an id number to each individual in the survey #### ----------------
 salvador_relevant_20 <- salvador_relevant_20 %>%
@@ -55,8 +61,22 @@ salvador_relevant_20 <- salvador_relevant_20 %>%
                             labels = c("Primary (or less)", "High School", 
                                        "BS", "Graduate", "Special Ed.")))
 
+salvador_relevant_10 <- salvador_relevant_10 %>%
+  # add age and education level categories
+  mutate(edad_factor = cut(edad, 
+                           breaks = c(-Inf, 13, 24, 44, 64, Inf),
+                           labels = c("Under 14", "14 to 24", "25 to 44",
+                                      "45 to 64", "65 and over")),
+         nivel_factor = cut(nivel,
+                            breaks = c(-Inf, 2, 3, 4, 5, Inf),
+                            labels = c("Primary (or less)", "High School", 
+                                       "BS", "Graduate", "Special Ed.")))
+
 #### Create survey design object to easily use weights and factor variables ####
 survey_salvador_20 <- salvador_relevant_20 %>%
+  as_survey_design(ids = id)
+
+survey_salvador_10 <- salvador_relevant_10 %>%
   as_survey_design(ids = id, 
                    weight = factor)
 
@@ -73,22 +93,70 @@ salvador_sex_20 <- survey_salvador_20 %>%
   summarise(proportion = survey_mean(),
             total = survey_total()) %>%
   select(sexo, proportion)
-names(salvador_age_20) <- c("By sex", "2020")
+names(salvador_sex_20) <- c("By sex", "2020")
 
 salvador_edu_20 <- survey_salvador_20 %>%
   group_by(nivel_factor) %>%
   summarise(proportion = survey_mean(),
             total = survey_total()) %>%
   select(nivel_factor, proportion)
-names(salvador_age_20) <- c("By education", "2020")
+names(salvador_edu_20) <- c("By education", "2020")
+
+# 2010
+salvador_age_10 <- survey_salvador_10 %>%
+  group_by(edad_factor) %>%
+  summarise(proportion = survey_mean(),
+            total = survey_total()) %>%
+  select(edad_factor, proportion)
+names(salvador_age_10) <- c("By age", "2010")
+
+salvador_sex_10 <- survey_salvador_10 %>%
+  group_by(sexo) %>%
+  summarise(proportion = survey_mean(),
+            total = survey_total()) %>%
+  select(sexo, proportion)
+names(salvador_sex_10) <- c("By sex", "2010")
+
+salvador_edu_10 <- survey_salvador_10 %>%
+  group_by(nivel_factor) %>%
+  summarise(proportion = survey_mean(),
+            total = survey_total()) %>%
+  select(nivel_factor, proportion)
+names(salvador_edu_10) <- c("By education", "2010")
 
 # Make a list with tables for presenting them together
-list(salvador_age_20, salvador_sex_20, salvador_edu_20) %>%
-  kable()
+salvador_age <- merge(salvador_age_10, salvador_age_20)
+salvador_edu <- merge(salvador_edu_10, salvador_edu_20)
+salvador_sex <- merge(salvador_sex_10, salvador_sex_20)
+
+tables <- list(salvador_age, salvador_sex, salvador_edu)
+kable(tables, digits = 4) %>% kable_styling()
+
+kable(salvador_age, digits = 4) %>% kable_styling()
+kable(salvador_edu, digits = 4) %>% kable_styling()
+kable(salvador_sex, digits = 4) %>% kable_styling()
 
 #### END OF SCRIPT #### -------------------------------------------------------
 
 
+survey_salvador_10 %>%
+  filter(is.na(nivel_factor)) %>%
+  group_by(edad_factor) %>%
+  summarise(prop = survey_mean()) %>%
+  kable() %>%
+  kable_styling()
 
+survey_salvador_20 %>%
+  filter(is.na(nivel_factor)) %>%
+  group_by(edad_factor) %>%
+  summarise(prop = survey_total()) %>%
+  kable() %>%
+  kable_styling()
 
+survey_salvador_20 %>%
+  filter(ocupacion == 10) %>%
+  group_by(nivel_factor) %>%
+  summarise(count = survey_total()) %>%
+  kable() %>%
+  kable_styling()
 
